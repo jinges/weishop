177a6e0cf358055e6f99b2bd568360555c8f5fd0
unknown <fenglmf@MFHYN-20150205P.hyn.com>
2015-06-23 16:29:36 +0800

    goumaishijian

--- js/common.js
+++ js/common.js
@@ -1,16 +1,16 @@
-(function(){
-	var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
-	var recalc = function(){
-		var width = document.body.clientWidth;
-		if(!width){ return false;};
-		width = width > 640 ? 640 : width;
-		document.documentElement.style.fontSize = 10 * (width / 320) +"px";
-	}
-	window.addEventListener(resizeEvt, recalc, false);
-	document.addEventListener('DOMContentLoaded', recalc, false);
-})();
-
 $(function() {
+	(function(){
+		var resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+		var recalc = function(){
+			var width = document.body.clientWidth;
+			if(!width){ return false;};
+			width = width > 640 ? 640 : width;
+			document.documentElement.style.fontSize = 10 * (width / 320) +"px";
+		}
+		window.addEventListener(resizeEvt, recalc, false);
+		document.addEventListener('DOMContentLoaded', recalc, false);
+	})();
+
 	$("#search-icon").click(function() {
 		$(".search").addClass("show").find(".txt").focus();
 	});
@@ -18,26 +18,26 @@ $(function() {
 	$("#nav-icon").click(function() {
 		alert('1');
 	});
+
+	$(".minus").click(function(){
+		var countObj = $(this).next();
+		var count =  countObj.val()*1;
+		if(count == 1){
+			return false;
+		}
+		countObj.val(-- count);
+	});
+
+	$(".plus").click(function(){
+		var countObj = $(this).prev();
+		var count =  countObj.val()*1;
+
+		countObj.val(++ count);
+	});
+
+	$(".delcar").click(function(){
+		//异步移除订单
+		
+		$(this).parent().parent().remove();
+	})
 });
-
-// function openwin() { 
-//    var isInFullScreen = (document.fullScreenElement && document.fullScreenElement !==     null) ||    // alternative standard method  
-//             (document.mozFullScreen || document.webkitI*sFullScreen);
-
-//     var docElm = document.documentElement;
-//     if (!isInFullScreen) {
-
-//         if (docElm.requestFullscreen) {
-//             docElm.requestFullscreen();
-//         }
-//         else if (docElm.mozRequestFullScreen) {
-//             docElm.mozRequestFullScreen();
-//             alert("Mozilla entering fullscreen!");
-//         }
-//         else if (docElm.webkitRequestFullScreen) {
-//             docElm.webkitRequestFullScreen();
-//         }
-//     }
-// }
-
-// $("body").click(openwin);
